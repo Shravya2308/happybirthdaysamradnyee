606afc4cdbc8ee5477d5e24049efa5c25aefdd30
Shravya2308 <shravyashetty2308@gmail.com>
2023-03-13 11:34:16 +0530

    almost doen
    almost there

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let dib = document.getElementById('hello');
+let bday_sound = document.querySelector(".resize");
+bday_sound.addEventListener("click", ding);
+
+function ding() {
+    var sound = new  Audio("piano.mp3");  
+    sound.play();
+}
+
+let hello;
+async function spells(){
+    console.log('hello')
+    const response = await fetch('https://hp-api.onrender.com/api/spells');
+    const all_spells = await response.json();
+    hello = Math.floor(Math.random() * all_spells.length);
+    console.log(hello);
+    return all_spells;
+}
+
+function soham(){
+    let a = spells();
+    console.log(a);
+    a.then(data => dib.innerHTML =data[hello].name)
+    
+}
